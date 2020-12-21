cfaa2b9af518b902760430f94aca95bea30c8c89
Leon Dmello <ldmello1@bloomberg.net>
2020-12-20 22:52:28 -0500

    Remove unused features

--- lib/backup.rb
+++ lib/backup.rb
@@ -37,16 +37,9 @@ module Backup
   module Storage
     autoload :Base,       File.join(STORAGE_PATH, "base")
     autoload :Cycler,     File.join(STORAGE_PATH, "cycler")
-    autoload :S3,         File.join(STORAGE_PATH, "s3")
-    autoload :CloudFiles, File.join(STORAGE_PATH, "cloud_files")
-    autoload :Ninefold,   File.join(STORAGE_PATH, "ninefold")
-    autoload :Dropbox,    File.join(STORAGE_PATH, "dropbox")
     autoload :FTP,        File.join(STORAGE_PATH, "ftp")
-    autoload :SFTP,       File.join(STORAGE_PATH, "sftp")
     autoload :SCP,        File.join(STORAGE_PATH, "scp")
-    autoload :RSync,      File.join(STORAGE_PATH, "rsync")
     autoload :Local,      File.join(STORAGE_PATH, "local")
-    autoload :Qiniu,      File.join(STORAGE_PATH, "qiniu")
   end
 
   ##
@@ -71,13 +64,7 @@ module Backup
   # Autoload Backup database files
   module Database
     autoload :Base,       File.join(DATABASE_PATH, "base")
-    autoload :MySQL,      File.join(DATABASE_PATH, "mysql")
     autoload :PostgreSQL, File.join(DATABASE_PATH, "postgresql")
-    autoload :MongoDB,    File.join(DATABASE_PATH, "mongodb")
-    autoload :Redis,      File.join(DATABASE_PATH, "redis")
-    autoload :Riak,       File.join(DATABASE_PATH, "riak")
-    autoload :OpenLDAP,   File.join(DATABASE_PATH, "openldap")
-    autoload :SQLite,     File.join(DATABASE_PATH, "sqlite")
   end
 
   ##
@@ -102,19 +89,7 @@ module Backup
   module Notifier
     autoload :Base,      File.join(NOTIFIER_PATH, "base")
     autoload :Mail,      File.join(NOTIFIER_PATH, "mail")
-    autoload :Twitter,   File.join(NOTIFIER_PATH, "twitter")
-    autoload :Campfire,  File.join(NOTIFIER_PATH, "campfire")
-    autoload :Prowl,     File.join(NOTIFIER_PATH, "prowl")
-    autoload :Hipchat,   File.join(NOTIFIER_PATH, "hipchat")
-    autoload :PagerDuty, File.join(NOTIFIER_PATH, "pagerduty")
-    autoload :Pushover,  File.join(NOTIFIER_PATH, "pushover")
-    autoload :Slack,     File.join(NOTIFIER_PATH, "slack")
     autoload :HttpPost,  File.join(NOTIFIER_PATH, "http_post")
-    autoload :Nagios,    File.join(NOTIFIER_PATH, "nagios")
-    autoload :FlowDock,  File.join(NOTIFIER_PATH, "flowdock")
-    autoload :Zabbix,    File.join(NOTIFIER_PATH, "zabbix")
-    autoload :DataDog,   File.join(NOTIFIER_PATH, "datadog")
-    autoload :Ses,       File.join(NOTIFIER_PATH, "ses")
     autoload :Command,   File.join(NOTIFIER_PATH, "command")
   end
 
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -540,12 +540,11 @@ describe "Backup::CLI" do
 
     it "should include the correct option values" do
       options = <<-EOS.lines.to_a.map(&:strip).map { |l| l.partition(" ") }
-        databases (mongodb, mysql, openldap, postgresql, redis, riak)
-        storages (cloud_files, dropbox, ftp, local, qiniu, rsync, s3, scp, sftp)
-        syncers (cloud_files, rsync_local, rsync_pull, rsync_push, s3)
+        databases (postgresql)
+        storages (local, scp)
         encryptor (gpg, openssl)
         compressor (bzip2, custom, gzip)
-        notifiers (campfire, command, datadog, flowdock, hipchat, http_post, mail, nagios, pagerduty, prowl, pushover, ses, slack, twitter)
+        notifiers (command, http_post, mail)
       EOS
 
       out, err = capture_io do
